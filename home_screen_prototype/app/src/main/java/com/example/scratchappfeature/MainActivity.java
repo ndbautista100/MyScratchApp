f26337abeb0f33285b7be6d84affabf5fe14b2a7
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2021-11-16 14:43:23 -0800

    home_screen basecode

---
+++ home_screen_prototype/app/src/main/java/com/example/scratchappfeature/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.scratchappfeature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
