a0136bdbf014aa0031c712d106e34c0305be4988
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-06 16:26:02 -0800

    updating the main folder

---
+++ login_screen/app/src/main/java/com/example/scratchappfeature/CreateRecipeActivity.java
@@ -0,0 +1,150 @@
+package com.example.scratchappfeature;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import org.w3c.dom.Text;
+
+import classes.AddIngredientDialogFragment;
+import classes.AddToolDialogFragment;
+import classes.Recipe;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+public class CreateRecipeActivity extends AppCompatActivity implements AddToolDialogFragment.AddToolDialogListener, AddIngredientDialogFragment.AddIngredientDialogListener {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private ImageButton addToolImageButton;
+    private ImageButton addIngredientImageButton;
+    private TextView toolsTextView;
+    private TextView ingredientsTextView;
+    private EditText recipeNameEditText;
+    private EditText toolNameEditText;
+    private Button doneButton;
+    private Recipe recipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+        Toolbar toolbarCreateRecipe = (Toolbar) findViewById(R.id.toolbarCreateRecipe);
+        setSupportActionBar(toolbarCreateRecipe);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        recipeNameEditText = (EditText) findViewById(R.id.recipeNameEditText);
+
+        toolsTextView = (TextView) findViewById(R.id.toolsTextView);
+        ingredientsTextView = (TextView) findViewById(R.id.ingredientsTextView);
+
+        addToolImageButton = (ImageButton) findViewById(R.id.addToolImageButton);
+        addToolImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddToolDialog();
+            }
+        });
+
+        addIngredientImageButton = (ImageButton) findViewById(R.id.addIngredientImageButton);
+        addIngredientImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddIngredientDialog();
+            }
+        });
+
+        doneButton = (Button) findViewById(R.id.doneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(recipeNameEditText.getText().toString())) {
+                    recipeNameEditText.setError("Please enter your recipe's name.");
+                    return;
+                }
+                recipe = new Recipe(recipeNameEditText.getText().toString());
+                recipe.setTools(toolsTextView.getText().toString());
+                recipe.setIngredients(ingredientsTextView.getText().toString());
+
+                // open recipe page and add recipe to database
+                openRecipePageActivity(recipe);
+            }
+        });
+    }
+
+    public void showAddToolDialog() {
+        AddToolDialogFragment dialog = new AddToolDialogFragment();
+        dialog.show(getSupportFragmentManager(), "AddToolDialogFragment");
+    }
+
+    @Override
+    public void applyToolName(String toolName) {
+        toolsTextView.setText(toolName);
+    }
+
+    public void showAddIngredientDialog() {
+        AddIngredientDialogFragment dialog = new AddIngredientDialogFragment();
+        dialog.show(getSupportFragmentManager(), "AddIngredientDialogFragment");
+    }
+
+    @Override
+    public void applyIngredientName(String ingredientName) {
+        ingredientsTextView.setText(ingredientName);
+    }
+
+    public void openRecipePageActivity(Recipe recipe) {
+        // parse Recipe object to Map
+        ObjectMapper oMapper = new ObjectMapper();
+        Map<String, Object> recipeMap = oMapper.convertValue(recipe, Map.class);
+
+
+        // add recipe to database
+        db.collection("recipes")
+            .add(recipeMap)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d("Success", "DocumentSnapshot added with ID: " + documentReference.getId());
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("Failure", "Error adding document", e);
+                }
+            });
+
+        Intent intent = new Intent(getApplicationContext(), RecipePageActivity.class);
+        intent.putExtra("recipe", recipe);
+        startActivity(intent);
+    }
+}
--- login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
@@ -7,48 +7,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "myTag";
-    private TextView login;
-    EditText userEmail, userPass;
-    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
         setSupportActionBar(toolbar);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,13 +30,26 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void logout(View v){
-        // logout function
-        // obtain the firebase instance and call sign out
-        FirebaseAuth.getInstance().signOut();
-
-        // start the login activity again since we are logged out
-        startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_create:
+                openScratchNotesActivity();
+                return true;
+            case R.id.action_search:
+                return true;
+            case R.id.action_profile:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
+    public void openScratchNotesActivity() {
+        Intent intent = new Intent(this, ScratchNotesActivity.class);
+        startActivity(intent);
+    }
+
 }
