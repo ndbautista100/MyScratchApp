8bad7743b8e0953a5cc7a965a15da82b41aa736d
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-06 17:08:07 -0800

    login integrated with scratch notes

--- login_screen/app/src/main/java/com/example/scratchappfeature/Register.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/Register.java
@@ -28,8 +28,8 @@ public class Register extends AppCompatActivity {
     FirebaseAuth fAuth;
 
     @Override
-    protected void onCreate (Bundle savedInstanceStats){
-        super.onCreate(savedInstanceStats);
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         mFullName           = findViewById(R.id.editTextTextRegisterName);
