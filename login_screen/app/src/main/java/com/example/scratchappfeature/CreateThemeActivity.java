9ac03be94bd083beedaeb34b7dc86265b7647a28
Sarah <sarah.ernst@student.csulb.edu>
2022-02-10 14:35:48 -0800

    CreateTheme Activity created
    main activity for the fragment buttons made

---
+++ login_screen/app/src/main/java/com/example/scratchappfeature/CreateThemeActivity.java
@@ -0,0 +1,14 @@
+package com.example.scratchappfeature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_theme);
+    }
+}
