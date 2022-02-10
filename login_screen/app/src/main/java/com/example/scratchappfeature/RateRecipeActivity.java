bec1b43690873a00f2635e21e9bea3b24853da59
Sarah <sarah.ernst@student.csulb.edu>
2022-02-10 14:16:39 -0800

    Created RateRecipe Activity
    Need to fix constraints on text, add simple bg & add way to record rating record

---
+++ login_screen/app/src/main/java/com/example/scratchappfeature/RateRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.scratchappfeature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RateRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_recipe);
+    }
+}
