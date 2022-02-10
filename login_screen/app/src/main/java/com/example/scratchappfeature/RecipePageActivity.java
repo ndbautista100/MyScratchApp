dbd693509fef5df3d7a004b6ed82568c7c2866c1
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-10 15:27:10 -0800

    added functionality for the image to be added to a recipe and then saved in the database
    need to add comments and clean up the code

--- login_screen/app/src/main/java/com/example/scratchappfeature/RecipePageActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/RecipePageActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -21,10 +26,17 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.OnProgressListener;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import java.util.UUID;
 
 import classes.Recipe;
 
@@ -36,6 +48,16 @@ public class RecipePageActivity extends AppCompatActivity {
     private TextView toolsTextView;
     private TextView ingredientsTextView;
     private int SELECT_PICTURE = 200;
+    // Uri indicates, where the image will be picked from
+    private Uri filePath;
+    // view for image view
+    private ImageView imageView;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +67,10 @@ public class RecipePageActivity extends AppCompatActivity {
         setSupportActionBar(toolbarScratchNotes);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+        // get the Firebase  storage reference
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
 
         // getting recipe sent from CreateRecipeActivity
         Intent intent = getIntent();
@@ -55,8 +81,9 @@ public class RecipePageActivity extends AppCompatActivity {
         toolsTextView.setText(recipe.getTools());
         ingredientsTextView = (TextView) findViewById(R.id.ingredientsTextViewRecipePage);
         ingredientsTextView.setText(recipe.getIngredients());
+        imageView = findViewById(R.id.imageView);
 
-        // database snippet - everythinig below here doesn't work yet
+        // database snippet - everything below here doesn't work yet
         Map<String, Object> recipeMap = new HashMap<>();
         recipeMap.put("name", recipe.getName());
 
@@ -67,6 +94,7 @@ public class RecipePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 imageChooser();
+                uploadImage();
             }
         });
 
@@ -76,6 +104,108 @@ public class RecipePageActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+
+    }
+
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContentResolver(),
+                                filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // UploadImage method
+    private void uploadImage()
+    {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref
+                    = storageReference
+                    .child(
+                            "images/"
+                                    + UUID.randomUUID().toString());
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot)
+                                {
+
+                                    // Image uploaded successfully
+                                    // Dismiss dialog
+                                    progressDialog.dismiss();
+                                    Toast
+                                            .makeText(RecipePageActivity.this,
+                                                    "Image Uploaded!!",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast
+                                    .makeText(RecipePageActivity.this,
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
+        }
     }
 
     /*
