2cc1586895d00e2c0ed93c975ed3b112a6169be8
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-17 15:31:07 -0800

    Revert "merge update from main into createTheme"
    This reverts commit 6c04f989476dc6c7c2ce9edde77ed53fd323bba8, reversing
    changes made to 916661569af1ec1d11e001c37c09350f77033b0e.

--- login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
@@ -7,50 +7,49 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private static final String TAG = "myTag";
+import java.util.HashMap;
+import java.util.Map;
 
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myTag";
+    private TextView login;
+    EditText userEmail, userPass;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        db.collection("recipes")
-                .whereEqualTo("user_ID", FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("Success", FirebaseAuth.getInstance().getCurrentUser().getUid() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d("err", "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+
     }
 
-<<<<<<< HEAD
     // TEST TEST comment
-=======
->>>>>>> main
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -58,34 +57,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.action_create:
-                openScratchNotesActivity();
-                return true;
-            case R.id.action_search:
-                return true;
-            case R.id.action_profile:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    public void logout(View v){
+        // logout function
+        // obtain the firebase instance and call sign out
+        FirebaseAuth.getInstance().signOut();
+
+        // start the login activity again since we are logged out
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
-
-    public void openScratchNotesActivity() {
-        Intent intent = new Intent(this, ScratchNotesActivity.class);
-        startActivity(intent);
-    }
-
-    public void logout(View view){
-    FirebaseAuth.getInstance().signOut();
-    startActivity(new Intent(getApplicationContext(), Login.class));
-    finish();
-    }
-
-
-
 }
