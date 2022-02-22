64d5ef0e9350905afffc8ec7d5b77ce5dd14446b
Sarah <sarah.ernst@student.csulb.edu>
2022-02-22 14:49:07 -0800

    java activity code update
    Added temp bg, RateRecipeActivity.java now has onCickListener to reflect actual rating.

--- login_screen/app/src/main/java/com/example/scratchappfeature/RateRecipeActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/RateRecipeActivity.java
@@ -1,8 +1,12 @@
 package com.example.scratchappfeature;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class RateRecipeActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class RateRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_recipe);
+        Button getRating = findViewById(R.id.btn_getRating);
+        final RatingBar ratingBar = findViewById(R.id.ratingBar);
+        getRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating = "Rating is :" + ratingBar.getRating();
+                Toast.makeText(RateRecipeActivity.this, rating, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
