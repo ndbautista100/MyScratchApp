1098cb3c58a7121ef142ce2143ebbfa16fba8ae7
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-25 10:00:34 -0800

    Activity class for sharing a video

---
+++ login_screen/app/src/main/java/com/example/scratchappfeature/AddVideoActivity.java
@@ -0,0 +1,12 @@
+package com.example.scratchappfeature;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddVideoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.id.activity_add_videos);
+    }
+}
