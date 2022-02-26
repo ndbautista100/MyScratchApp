c40e8dcf78a92f22b98ab2c155335d86fa682c64
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-25 22:19:28 -0800

    initializing the view items for adding a video activity

--- login_screen/app/src/main/java/com/example/scratchappfeature/AddVideoActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/AddVideoActivity.java
@@ -1,12 +1,41 @@
 package com.example.scratchappfeature;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AddVideoActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+    private Button uploadBtn;
+    private EditText eT;
+    private VideoView videoView;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.id.activity_add_videos);
+        setContentView(R.layout.activity_add_videos);
+
+        uploadBtn = findViewById(R.id.uploadVideo);
+        eT =  findViewById(R.id.titleEt);
+        videoView = findViewById(R.id.videoView);
+        fab = findViewById(R.id.pickVideo);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
