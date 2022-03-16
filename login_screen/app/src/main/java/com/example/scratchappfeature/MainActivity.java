7e712339e2408d9a81a34f35ab6126fcba2498ae
MichaelAnderson1128 <80723738+MichaelAnderson1128@users.noreply.github.com>
2022-03-16 16:46:23 -0700

    Added the viewOtherRecipe activity and class just to view the recipes on the main feed. This will also allow us to start saving/sharing the recipe the user database

--- login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
+++ login_screen/app/src/main/java/com/example/scratchappfeature/MainActivity.java
@@ -195,6 +195,12 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("open_recipe_from_id", recipe_ID);
         startActivity(intent);
     }
+
+    public void openOtherUserRecipe(String recipe_ID){
+        Intent intent = new Intent(getApplicationContext(), ViewOtherRecipe.class);
+        intent.putExtra("open_recipe_from_id", recipe_ID);
+        startActivity(intent);
+    }
     public void openProfilePageActivity() {
         Intent intent = new Intent(this, ProfilePage.class);
         startActivity(intent);
